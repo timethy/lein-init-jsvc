06fb63f3cb7fed6ea46d93fbbaed865bf56db769
Homer Strong <homer.strong@gmail.com>
2012-05-09 15:54:59 -0700

    add to clojars group

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject leiningen-init-script "1.2.0-SNAPSHOT"
+(defproject org.clojars.strongh/leiningen-init-script "1.2.0-SNAPSHOT"
   :description "A leiningen plugin that allows you to generate *NIX init scripts."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]])
