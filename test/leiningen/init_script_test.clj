4e142bb8a30310db16ed44205c5170ea7c87c8ed
David Park <daviddpark@gmail.com>
2012-12-18 11:28:33 -0500

    Added init_script_test for some unit testing

---
+++ test/leiningen/init_script_test.clj
@@ -0,0 +1,19 @@
+(ns leiningen.init-script-test
+  (:use clojure.test
+        leiningen.init-script))
+;; Testing options
+(def test-opts {:properties {:clj-config.env "dev"
+			     :java.library.path "/some/dir"}
+		:jvm-opts ["-server" 
+			    "-Xms1G"
+			    "-Xmx2G"
+			    "-XX:MaxPermSize=128M"]})
+
+
+
+(deftest init-script-tests
+  (testing "format-properties"
+    (is (= (format-properties {:properties {:foo "bar"}})  "-Dfoo=\"bar\"")))
+  (testing "format-java-string"
+    (is (= (format-java-string test-opts) "-Djava.library.path=\"/some/dir\" -Dclj-config.env=\"dev\" -server -Xms1G -Xmx2G -XX:MaxPermSize=128M")))
+  )
