dcc63664b08af64ce12475735f506ac1c6b194f3
Homer Strong <homer@luckysort.com>
2012-12-19 11:36:28 -0800

    unsnapshot version

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject org.clojars.strongh/lein-init-script "1.3.1-SNAPSHOT"
+(defproject org.clojars.strongh/lein-init-script "1.3.1"
   :description "A leiningen plugin that allows you to generate *NIX init scripts."
   :eval-in-leiningen true)
