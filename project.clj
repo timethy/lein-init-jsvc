76a0469ef089b77110b4f57bebf1cf62c6a416dc
timethy <taubnert@student.ethz.ch>
2014-04-12 02:44:32 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-init-jsvc "0.2.4"
+(defproject lein-init-jsvc "0.2.5"
   :description "A leiningen plugin that allows you to generate *NIX init scripts."
   :eval-in-leiningen true
   :dependencies [[lein-tar "3.1.0"]])
